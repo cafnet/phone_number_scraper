42433247726b30326543111906195c818d122878
Jeffrey Quinn <jeffreyquinn@Jeffreys-MacBook-Air.local>
2013-11-10 10:41:03 -0800

    update

--- phone_number_scraper.py
+++ phone_number_scraper.py
@@ -1,6 +1,5 @@
 import argparse
 import sys
-import csv
 import logging
 import urlparse
 import re
@@ -8,23 +7,10 @@ from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 import selenium.common.exceptions
 
-parser = argparse.ArgumentParser(description='Scrape phone numbers from links.')
-parser.add_argument('--csv', dest='inputfn', type=str,
-                    help="Input spreadsheets with links.")
-parser.add_argument('--log', type=str, dest='logfile', default='scraper.log',
-                    help="Log destination")
-parser.add_argument('--out', dest='outputfn', type=str,
-                    help="Input spreadsheets with links.")
-parser.add_argument('--maxlinks', dest='max_links', type=int, default=10,
-                    help="How many links to crawl through from the site's home page until quitting.")
-parser.add_argument('--wait', dest='wait', type=int, default=60,
-                    help="How many seconds to wait for a page to load until giving up.")
-args = parser.parse_args()
-
 ##set up logging
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-fh = logging.FileHandler(args.logfile)
+fh = logging.FileHandler("scraper.log")
 fh.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.ERROR)
@@ -58,6 +44,7 @@ priority_re = re.compile(priority_re, re.IGNORECASE)
 ##regexp that matches a phone number of any format
 phone_re = ur'(?:(?<=[\s\:\(])|\A)(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?'
 phone_re = re.compile(phone_re, re.UNICODE)
+
 def number_finder(text):
     assert isinstance(text, basestring)
     return phone_re.findall(text)
@@ -68,19 +55,12 @@ class PhoneScraper:
     It will emit a new spreadsheet appending the phone numbers it finds
     at each URL to the appropriate row of the spreadsheet
     """
-    def __init__(self, ifn, ofn, max_links = 10, wait = 60):
-	self.ifn = ifn
-	self.ofn = ofn
-	self.numbers = {}
+    def __init__(self, max_links = 10, wait = 60):
 	self.max_links = max_links
 
 	self.main_driver = webdriver.Firefox()
 	self.main_driver.implicitly_wait(wait)
 	logger.info("Webdriver successfully initiated.")
-
-	self.read_csv()
-	self.outputcsv = open(ofn, 'w')
-	self.writer = csv.writer(self.outputcsv)
 
     def is_internal(self, url):
 	"""
@@ -93,7 +73,6 @@ class PhoneScraper:
 	    return True
 	else:
 	    return False
-
 
     def link_priority(self, ele):
 	"""
@@ -113,35 +92,6 @@ class PhoneScraper:
 	    return 1
 	else:
 	    return 2
-
-    def read_csv(self):
-	"""
-	Read in the input CSV that contains the URLs to visit.
-	Assumes the URLs are in the first column of the spreadsheet.
-	"""
-	self.input_data = {}
-	with open(self.ifn, 'rb') as csvfile:
-	    logger.debug("Opened %s.", args.inputfn)
-	    dialect = csv.Sniffer().sniff(csvfile.read(1024))
-	    csvfile.seek(0)
-	    reader = csv.reader(csvfile, dialect=dialect)
-	    for row in reader:
-		self.input_data[row[0]] = row
-	    logger.debug("There are (%s) entries to process." % len(self.input_data) )
-
-    def update_csv(self):
-	"""
-	Write all URL/phone number pairs that exist in self.numbers to the output spreadsheet.
-	"""
-	scraped_numbers = self.numbers.items()
-	for k,v in scraped_numbers:
-	    row = self.input_data[k] 
-	    if isinstance(v, list):
-		row.extend(v)
-	    else:
-		row.append('None Found')
-	    self.writer.writerow(row )
-	    del self.numbers[k]
 
     def scrape_phone_numbers(self):
 	"""
@@ -199,54 +149,43 @@ class PhoneScraper:
 	logger.info("(%s) links on page" % len(internal_links) )
 	return [ x[0] for x in internal_links ]
 
-    def find_numbers(self):
+    def find_numbers(self, raw_url):
 	"""
 	Iterate through input spreadsheet, load a URL from the spreadsheet, open it in the browser, try to extract phone numbers.
 	If that fails, follow links from the homepage until we find a page with a phone number on it, then quit.
 	Emit all found numbers to the output spreadsheet.
 	"""
-	for raw_url in self.input_data.keys():
-	    url = normalize_url(raw_url)
-	    logger.info("Processing (%s)." % (url) )
+	url = normalize_url(raw_url)
+	logger.info("Processing (%s)." % (url) )
+	try:
+	    self.main_driver.get(url)
+	except selenium.common.exceptions.WebDriverException:
+	    logger.info("Failed to GET (%s)." % (url) )
+	    return None
+	except Exception as e:
+	    logger.error("Exception (%s) raised when trying to access (%s)" % (e, link) )
+	    return None
+	initial_page_numbers = self.scrape_phone_numbers()
+	if initial_page_numbers: return initial_page_numbers
+	links = self.yield_links()
+	links_tried = 0
+	for link in links:
+	    links_tried += 1
 	    try:
-		self.main_driver.get(url)
+		self.main_driver.get(link)
 	    except selenium.common.exceptions.WebDriverException:
-		logger.info("Failed to GET (%s)." % (url) )
-		self.numbers[raw_url] = ['Could not access page']
-		self.update_csv()
+		logger.info("Failed to GET (%s)." % (link) )
 		continue
 	    except Exception as e:
 		logger.error("Exception (%s) raised when trying to access (%s)" % (e, link) )
 		continue
-	    self.numbers[raw_url] = self.scrape_phone_numbers()
-	    if self.numbers[raw_url]: 
-		self.update_csv()
-		continue
-	    links = self.yield_links()
-	    links_tried = 0
-	    for link in links:
-		links_tried += 1
-		try:
-		    self.main_driver.get(link)
-		except selenium.common.exceptions.WebDriverException:
-		    logger.info("Failed to GET (%s)." % (link) )
-		    continue
-		except Exception as e:
-		    logger.error("Exception (%s) raised when trying to access (%s)" % (e, link) )
-		    continue
-		logger.info("Spider crawling to (%s) from (%s)." % ( link, url) )
-		self.numbers[raw_url] = self.scrape_phone_numbers()
-		if self.numbers[raw_url]: break
-		if links_tried >= self.max_links: break
-	    self.update_csv()
+	    logger.info("Spider crawling to (%s) from (%s)." % ( link, url) )
+	    page_numbers = self.scrape_phone_numbers()
+	    if page_numbers: return page_numbers
+	    if links_tried >= self.max_links: return None
+	return None
 
     def __enter__(self): return self
+
     def __exit__(self, type, value, traceback):
 	self.main_driver.close()
-	self.outputcsv.close()
-
-if __name__ == '__main__': 
-    with PhoneScraper(args.inputfn, args.outputfn, max_links = args.max_links, wait = args.wait) as scraper:
-	scraper.find_numbers()
-
-
